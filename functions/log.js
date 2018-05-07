2873fbea4e2183d378cd2633ac69bae8d96c3a6a
Bryce Kahle <bkahle@gmail.com>
2018-05-07 14:08:33 -0400

    log parameters in test function

--- functions/log.js
+++ functions/log.js
@@ -1,3 +1,5 @@
 exports.handler = function(event, context, callback) {
-  console.log("log function called at " + Date.now());
+  console.log("log function called at", Date.now());
+  console.log("event", event);
+  console.log("context", context);
 };
