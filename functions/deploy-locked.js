b7d34f49ab45e730f8c47794d2612f048ce600dd
Bryce Kahle <bkahle@gmail.com>
2018-05-18 14:16:52 -0400

    In reality, this only gets triggered for locks. Separate function for unlocks.

--- functions/deploy-locked.js
+++ functions/deploy-locked.js
@@ -1,10 +1,6 @@
 exports.handler = function(event, context, callback) {
   var body = JSON.parse(event.body);
-  if (body.payload.locked) {
-    console.log("deploy locked to", body.payload.commit_ref);
-  } else {
-    console.log("deploy unlocked");
-  }
+  console.log("deploy locked to", body.payload.commit_ref);
   callback(null, {
     statusCode: 200
   });
