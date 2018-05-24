607a2812df91f2bd1d0aa2324a6aa826d242bb85
Bryce Kahle <bkahle@gmail.com>
2018-05-24 17:27:50 -0400

    Introduce randomness of error or success response

--- go/go-error.go
+++ go/go-error.go
@@ -3,12 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	ts := time.Now().Nanosecond()
+	if ts%2 == 0 {
+		return &events.APIGatewayProxyResponse{StatusCode: 200, Body: "success"}, nil
+	}
 	return nil, fmt.Errorf("function error")
 }
 
