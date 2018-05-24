37f6b9a119a1b06040e4178f0dc16b65c2ee8e97
Bryce Kahle <bkahle@gmail.com>
2018-05-24 15:40:05 -0400

    Add go error function

---
+++ functions/go-error.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return nil, fmt.Errorf("function error")
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
+}
